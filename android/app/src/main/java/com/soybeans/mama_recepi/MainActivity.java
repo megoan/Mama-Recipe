82d1b04c5d6d157e3fb18d29d5a9962d3df0f7f6
megoan <shmuelsoib@gmail.com>
2019-08-30 11:47:48 +0300

    First Commit

---
+++ android/app/src/main/java/com/soybeans/mama_recepi/MainActivity.java
@@ -0,0 +1,13 @@
+package com.soybeans.mama_recepi;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
